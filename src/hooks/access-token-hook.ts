32127063f2c5b5ea26ed58bf51a60e890d38664f
Morgan Walkup <morganwalkup@gmail.com>
2026-04-30 14:49:21 -0500

    Apply suggestion from @morganwalkup

--- src/hooks/access-token-hook.ts
+++ src/hooks/access-token-hook.ts
@@ -34,8 +34,7 @@ const isBrowserLike = webWorkerLike
  */
 export class AccessTokenHook implements SDKInitHook, BeforeRequestHook {
   sdkInit(opts: SDKOptions): SDKOptions {
-    // @ts-expect-error - accessToken will be available after sdk generation
-    const token = opts.accessToken;
+    const token = (opts as SDKOptions & { accessToken: string }).accessToken;
     const hasToken = typeof token === "string" && token.length > 0;
 
     if (hasToken && opts.security != null) {
