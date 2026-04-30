5bd967580df20bd761125cd860decd213ef17fdf
Morgan Walkup <morganwalkup@gmail.com>
2026-04-30 12:25:53 -0500

    Get accessToken hook working as expected

--- src/hooks/registration.ts
+++ src/hooks/registration.ts
@@ -18,4 +18,5 @@ export function initHooks(hooks: Hooks) {
 
   const accessTokenHook = new AccessTokenHook();
   hooks.registerSDKInitHook(accessTokenHook);
+  hooks.registerBeforeRequestHook(accessTokenHook);
 }
